ac06d5ad4f6282aa4c955812edb11f3d772f39b2
emitolaba95 <emi.tolaba95@gmail.com>
2019-11-02 00:12:53 -0300

    Agregando calificacion a listado de compras

--- app/src/main/java/ar/edu/utn/frba/mobile/smarket/adapters/PurchasesAdapter.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/smarket/adapters/PurchasesAdapter.kt
@@ -1,11 +1,12 @@
 package ar.edu.utn.frba.mobile.smarket.adapters
 
 import android.app.ActionBar
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import ar.edu.utn.frba.mobile.smarket.R
@@ -14,8 +15,6 @@ import ar.edu.utn.frba.mobile.smarket.model.Purchase
 import ar.edu.utn.frba.mobile.smarket.service.PurchaseService
 import kotlinx.android.synthetic.main.item_purchase.view.*
 import java.text.SimpleDateFormat
-import android.widget.FrameLayout
-import android.widget.RelativeLayout
 import ar.edu.utn.frba.mobile.smarket.service.RatingService
 
 class PurchasesAdapter(
@@ -38,61 +37,83 @@ class PurchasesAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val purchase : Purchase = dataSet[position]
+        holder.bindData(purchase)
 
-        holder.itemView.textDescription.text = "$ " + purchase.price.toString()
-        holder.itemView.textProducts.text = purchase.amount.toString() + " productos"
-
-        holder.itemView.textDate.text = SimpleDateFormat("dd-MM-yyyy")
-            .format(purchase.date).toString()
-
-        holder.itemView.textHour.text = SimpleDateFormat("hh:mm")
-            .format(purchase.date).toString()
-
-        holder.itemView.textStatus.text = context.resources.getString(purchase.status.value)
-        holder.itemView.textStatus.setTextColor(ContextCompat.getColor(context, purchase.status.color))
-
-        holder.itemView.buttonRepeatPurchase.setOnClickListener {
-            actionRepeatPurchase(purchase)
-        }
-
-        if (purchase.status == PurchaseStatus.FINISHED) {
-            holder.itemView.buttonQualify.visibility = View.VISIBLE
-
-            holder.itemView.buttonQualify.setOnClickListener {
-                //holder.itemView.relativeLayoutRatingBar.layoutParams = RelativeLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT)
-                holder.itemView.relativeLayoutItemPurchase.layoutParams.width = 0
-                holder.itemView.relativeLayoutItemPurchase.layoutParams.height = 0
-            }
-
-            holder.itemView.buttonQualifyCancel.setOnClickListener {
-                //holder.itemView.relativeLayoutItemPurchase.layoutParams = RelativeLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT)
-                holder.itemView.relativeLayoutRatingBar.layoutParams.width = 0
-                holder.itemView.relativeLayoutRatingBar.layoutParams.height = 0
-            }
-
-            holder.itemView.buttonQualifyOk.setOnClickListener {
-                RatingService.qualify(purchase.uid, holder.itemView.ratingBar.rating)
-                purchase.status = PurchaseStatus.QUALIFIED
-                PurchaseService.updateStatus(purchase)
-               // holder.itemView.relativeLayoutItemPurchase.layoutParams = RelativeLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT)
-                holder.itemView.relativeLayoutRatingBar.layoutParams.width = 0
-                holder.itemView.relativeLayoutRatingBar.layoutParams.height = 0
-            }
-        }
-    }
-
-    private fun linearLayoutInvisible(linearLayout: View) {
-        linearLayout.layoutParams =
-            FrameLayout.LayoutParams(0, 0)
-    }
-
-    private fun linearLayoutVisible(linearLayout: View) {
-        linearLayout.layoutParams =
-            FrameLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT)
     }
 
     override fun getItemCount() = dataSet.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bindData(purchase: Purchase){
+            itemView.textDescription.text = "$ " + purchase.price.toString()
+            itemView.textProducts.text = purchase.amount.toString() + " productos"
+
+            itemView.textDate.text = SimpleDateFormat("dd-MM-yy hh:mm")
+                .format(purchase.date).toString()
+
+            setPurchaseTextStatus(purchase)
+            itemView.buttonRepeatPurchase.setOnClickListener {
+                actionRepeatPurchase(purchase)
+            }
+
+            when {
+                purchase.status == PurchaseStatus.FINISHED -> {
+                    setButtonQualifyVisibility()
+                    itemView.buttonQualify.setOnClickListener {
+
+                        val builder = AlertDialog.Builder(context)
+                        builder.setTitle("Califica esta compra")
+
+                        val ratingBar = View.inflate(context, R.layout.ratingbar, null)
+                        builder.setView(ratingBar)
+
+                        builder.setPositiveButton("Confirmar") { _, _ ->
+                            val number = ratingBar.findViewById<RatingBar>(R.id.ratingBar).rating
+                            RatingService.qualify(purchase.uid, number)
+                            purchase.status = PurchaseStatus.QUALIFIED
+
+                            PurchaseService.updateStatus(purchase)
+                            updateViewOnQualifyResult(purchase, number)
+                        }
+                        builder.create().show()
+                    }
+                }
+
+                purchase.status == PurchaseStatus.QUALIFIED -> {
+                    setButtonQualifyGone()
+                    //TODO: obtener rating
+                }
+
+                else -> setButtonQualifyAndStatusGone()
+            }
+        }
+
+        private fun updateViewOnQualifyResult(purchase: Purchase, number: Float){
+            itemView.qualifyResult.rating = number
+            setPurchaseTextStatus(purchase)
+            setButtonQualifyGone()
+        }
+
+        private fun setPurchaseTextStatus(purchase: Purchase){
+            itemView.textStatus.text = context.resources.getString(purchase.status.value)
+            itemView.textStatus.setTextColor(ContextCompat.getColor(context, purchase.status.color))
+        }
+
+        private fun setButtonQualifyVisibility(){
+            itemView.buttonQualify.visibility = View.VISIBLE
+            itemView.qualifyResult.visibility = View.GONE
+        }
+
+        private fun setButtonQualifyGone(){
+            itemView.buttonQualify.visibility = View.GONE
+            itemView.qualifyResult.visibility = View.VISIBLE
+        }
+
+        private fun setButtonQualifyAndStatusGone(){
+            itemView.buttonQualify.visibility = View.GONE
+            itemView.qualifyResult.visibility = View.GONE
+        }
+    }
 
 }
